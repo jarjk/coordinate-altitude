3b9f4c8c385c5a257d403bb32470a9002605b327
Jeromos Kovacs <iitsnotme214@proton.me>
2024-07-05 15:41:23 +0200

    initial commit

---
+++ src/lib.rs
@@ -0,0 +1,56 @@
+/// a coordinate
+#[derive(Clone, Copy, PartialEq, Default, Debug)]
+pub struct Coord {
+    /// x
+    latitude: f64,
+    /// y
+    longitude: f64,
+    /// elevation above sea-level
+    altitude: f64,
+}
+
+impl From<(f32, f32)> for Coord {
+    fn from(val: (f32, f32)) -> Self {
+        Coord::new(val.0, val.1)
+    }
+}
+
+impl Coord {
+    pub fn new<F1: Into<f64>, F2: Into<f64>>(latitude: F1, longitude: F2) -> Self {
+        Self {
+            latitude: latitude.into(),
+            longitude: longitude.into(),
+            altitude: 0.,
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn new() {
+        let result = Coord::new(44., 2);
+        assert_eq!(
+            result,
+            Coord {
+                latitude: 44.,
+                longitude: 2.,
+                altitude: 0.
+            }
+        );
+    }
+    #[test]
+    fn default() {
+        let result = Coord::default();
+        assert_eq!(
+            result,
+            Coord {
+                latitude: 0.,
+                longitude: 0.,
+                altitude: 0.
+            }
+        )
+    }
+}
